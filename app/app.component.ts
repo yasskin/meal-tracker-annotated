2cf0855ba98370317b8d3f62ebcc1b03aa707681
Noah <yasskin@icloud.com>
2016-08-07 18:15:43 -0700

    add working skeleton Angular2 app

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>New Skeleton Angular2 App!</h1>
+    </div>
   `
 })
 
