f531cc109e9f6949aceee32deb312ecddb8b14de
Noah <yasskin@icloud.com>
2016-08-11 18:04:10 -0700

    add AppComponent Controller class definition with template view and Meal class

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,25 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Cameron's Meal Tracker!</h1>
+      <h3 *ngFor="#meal of meals">{{ meal.name }}, {{ meal.description }}, {{ meal.calories }}</h3>
     </div>
   `
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Breakfast", "Soylent", 250, 0),
+      new Meal("Lunch", "Burger", 600, 1),
+      new Meal("Second Lunch", "Curry", 500, 2),
+      new Meal("Dinner", "Pasta", 700, 3)
+    ];
+  }
+}
 
+export class Meal {
+  constructor(public name: string, public details: string, public calories: number, public id: number) {
+
+  }
 }
